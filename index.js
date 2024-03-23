3b169e62a47ba1550341937e31266c8186b5c40f
robcannabis <157723006+robcannabis@users.noreply.github.com>
2024-03-23 23:12:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -35,11 +35,11 @@ client.on('ready', async () => {
     const created = moment().format('YYYY-MM-DD HH:mm:ss ');
     const change = ['https://cdn.discordapp.com/attachments/1065988274918797392/1215695340527943790/khuntae_rob.gif?ex=6606ea03&is=65f47503&hm=44824674c6e31825411d8afb6f4acb8902ad2462d42e7ed426314b261ccdd25a&']; //รูปใหญ่
      //const poop = ['https://cdn.discordapp.com/attachments/1114551993499918438/1122462459710681170/1095697715238797403.gif','https://cdn.discordapp.com/attachments/1114551993499918438/1122462460817977395/1095697740543049819.gif','https://cdn.discordapp.com/attachments/1114551993499918438/1122462460125921341/1095697719189844048.gif','https://cdn.discordapp.com/attachments/1114551993499918438/1116029023957561425/1110570045454696509.gif']; // รูปเล็ก
-    const change2 = ['@ RobShop','@ รับเติมไนโตร','@ Boost 15O ฿','@ Classic 75 ฿']; // ชื่อสลับ
+    const change2 = ['@ RobShop','@ รับเติมไนโตร','@ Boost 16O ฿','@ Classic 75 ฿']; // ชื่อสลับ
     const yyyt = ['꒰ ♯ chxxfs ♪ ꒱','꒰ ♯ babybigboy ♪ ꒱','꒰ ♯ cnxtown ♪ ꒱','꒰ ♯ maiyarap ♪ ꒱']; // ชื่อ button1
     const ddds = ['https://www.youtube.com/watch?v=Tmh_Ms8Yhgk','https://www.youtube.com/watch?v=UINIiEgtcz8','https://www.youtube.com/watch?v=VZXX0w7ij1I&t=80s','https://www.youtube.com/watch?t=10&v=BUD0m11eKMU&feature=youtu.be']; // ลิ้ง button1
 
-   const iooi = ['꒰ Join Server ꒱','꒰ RobShop ꒱','꒰ Boost 15O ฿ ꒱','꒰ Classic 75 ฿꒱']; // ชื่อ button 2
+   const iooi = ['꒰ Join Server ꒱','꒰ RobShop ꒱','꒰ Boost 16O ฿ ꒱','꒰ Classic 75 ฿꒱']; // ชื่อ button 2
    const iiio = ['https://discord.gg/n2YV89WksM','https://discord.gg/n2YV89WksM','https://discord.gg/n2YV89WksM','https://discord.gg/n2YV89WksM']; // ลิ้ง button 2
    const tyyy = yyyt[Math.floor(Math.random()*yyyt.length)]; // ชื่อ button 2
    const sddd = ddds[Math.floor(Math.random()*ddds.length)]; // ลิ้ง button 2
